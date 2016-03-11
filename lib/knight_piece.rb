7d1f591f49d11ff5829e2758a73ac63ef4b55350
Matthew Crawford <mcrawford0126@gmail.com>
2016-03-11 00:27:26 -0500

    corrects possible_knight_moves to return correct array of knight moves

--- lib/knight_piece.rb
+++ lib/knight_piece.rb
@@ -2,11 +2,12 @@ require_relative 'chess_board'
 require_relative 'chess_piece'
 class KnightPiece < ChessPiece
 
-  attr_accessor :piece
+  attr_accessor :piece, :config
 
-  def initialize(row, col)
+  def initialize(row, col, config)
     super(row,col)
-    @piece = "K"
+    @piece = 'K'
+    @config = config
   end
 
   def get_current_cell
@@ -22,20 +23,21 @@ class KnightPiece < ChessPiece
   def possible_knight_moves
     [[@row + 2, @col + 1], [@row + 2, @col - 1],
      [@row + 1, @col + 2], [@row + 1, @col - 2],
-     [@row - 1, @col + 2], [@row - 1, @col + 2],
-     [@row - 2, @col + 1], [@row - 2, @col + 1]]
+     [@row - 1, @col + 2], [@row - 1, @col - 2],
+     [@row - 2, @col + 1], [@row - 2, @col - 1]]
   end
 
   # gets all possible moves for a knight with a given board configuration
   # @config current ChessBoard configuration
   # @return an array of all possible knight move configurations
-  def get_neighbors(config)
+  def get_neighbors
     possible_move_configs = []
     possible_knight_moves.each do |move|
-      if canMove(config, move[0], move[1])
-        new_config = ChessBoard.new(config)
-        possible_move_config = new_config.move(config, move[0],move[1], @piece)
-        possible_move_configs.push(possible_move_config)
+      if can_move(@config, move[0], move[1])
+        new_config = ChessBoard.new(@config)
+        possible_move_config = new_config.move(@config, move[0],move[1], @piece)
+        knight = KnightPiece.new(move[0], move[1], possible_move_config)
+        possible_move_configs.push(knight)
       end
     end
     possible_move_configs
