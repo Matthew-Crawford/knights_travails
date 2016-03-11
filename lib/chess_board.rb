0cab552be5c4a4777b7625dcb29c0715ae0e0666
Matthew Crawford <mcrawford0126@gmail.com>
2016-03-11 00:25:29 -0500

    corrects array range for board init

--- lib/chess_board.rb
+++ lib/chess_board.rb
@@ -3,10 +3,11 @@ class ChessBoard
   attr_accessor :board, :row_size, :col_size
 
   def initialize(board = nil, row_size = 8, col_size = 8)
-    unless board.nil?
-      @board = Array.new(board)
+    if !board.nil?
+      @board = Array.new(board.board)
+    else
+      @board = Array.new(row_size) { Array.new(col_size) }
     end
-    @board = Array.new(row_size) { Array.new(col_size) }
     @row_size = row_size
     @col_size = col_size
   end
@@ -21,17 +22,24 @@ class ChessBoard
 
   #checks if the specified cell is within the board
   def validate_cell(row, col)
-    if (row > 8 || row < 0) || (col > 8 || col < 0)
+    if (row > 7 || row < 0) || (col > 7 || col < 0)
       return false
     end
     true
   end
 
   def build_board
-    @board.each do |row|
-      row.each do |col|
-        @board[row][col] = '.'
+    @board.each_with_index do |row, row_index|
+      row.each_with_index do |col, col_index|
+        @board[row_index][col_index] = '.'
       end
+    end
+  end
+
+  def build_finished_board(moves, piece)
+    moves.each_with_index do |move, index|
+      s = "#{piece}#{index.to_s}"
+      @board[move.row][move.col] = s
     end
   end
 
@@ -41,7 +49,7 @@ class ChessBoard
 
   def move(old_board, row, col, piece)
     new_board = ChessBoard.new(old_board)
-    new_board[row][col] = piece
+    new_board.board[row][col] = piece
+    new_board
   end
-
 end
