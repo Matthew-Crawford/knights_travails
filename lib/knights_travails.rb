94c8d3e8b05ccf33053e5e01dec728afcddc2995
Matthew Crawford <mcrawford0126@gmail.com>
2016-03-11 00:29:39 -0500

    changes array check to use == to check for equality

--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -5,24 +5,43 @@ require_relative 'knight_piece'
 class KnightsTravails
   attr_accessor :start, :end_goal
 
-  # checks if the end location has been found
-  def at_goal?(curr_location)
-    @end_goal.eql?(curr_location)
+  def initialize(start, end_goal)
+    @start = start
+    @end_goal = end_goal
   end
 
-  def run(start, end_goal)
+  # checks if the end location has been found
+  def at_goal?(curr_config)
+    @end_goal == [curr_config.row,curr_config.col]
+  end
+
+  def run
 
     @start = start
     @end_goal = end_goal
 
-    chess_board = ChessBoard.new
+    chess_board = ChessBoard.new(nil,5,5)
     chess_board.build_board
-    chess_board.initialize_piece(0,0,"K")
+    chess_board.initialize_piece(start[0],start[1],'K')
     solver = KnightSolver.new
 
     path = solver.knight_moves(@start,@end_goal)
+
+    path.each do |move|
+      puts "#{move.row}, #{move.col}"
+    end
+
+    chess_board.build_finished_board(path, 'K')
+
+    chess_board.row_size.times do |row|
+      chess_board.col_size.times do |col|
+        print chess_board.board[row][col]
+      end
+      puts
+    end
+
   end
 
-  KnightsTravails.new.run([0,0], [3,3])
+  KnightsTravails.new([3,3],[0,0]).run
 
 end
